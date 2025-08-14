4a449f65a41bb2ad95dcb024fbc1de55230cfe39
rupesh kumar <149963939+ru-kumar@users.noreply.github.com>
2025-08-15 00:21:02 +0530

    Your commit message

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL = "https://wanderlustbackend-production-c840.up.railway.app";
-export const API_BASE_URL = "https://wanderlustbackend-production-c840.up.railway.app/";
+
 // export const API_BASE_URL = "http://localhost:5354";
 
 export const api = axios.create({ 
