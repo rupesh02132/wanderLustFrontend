bab20cf3501a05a90db57bd8f3d1e12fd568643f
Rupesh kumar <rupeshkumar02132@gmail.com>
2025-10-07 14:52:12 +0530

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://wanderlustbackend-1.onrender.com/";
+export const API_BASE_URL = "https://wanderlustbackend-1.onrender.com";
+
 
 // export const API_BASE_URL = "http://localhost:5354";
 
